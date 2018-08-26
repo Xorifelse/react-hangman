cd75d808648ffc928bd80af2ded902d41b60b753
Xorifelse <johan.kruse.com>
2018-08-27 01:29:17 +0200

    changed some styling, added vowels

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -27,12 +27,19 @@ export default function Hangman(props) {
       <div className="main">
         <div className={"hangman pos" + props.count} />
         <div><h1>{props.show}</h1></div>
-        <div>Wrong guesses: {props.count}</div>
+        <div className="trys">
+          <div>Trys left</div>
+          <div>{ 6 - props.count}</div>
+        </div>
       </div>
       <div className="buttons">
-        {genAlphabeth().map(v =>
-          <button key={v} onClick={props.guessEvent} value={v}>{v}</button>
-        )}
+        {genAlphabeth().map(v => {
+          if(['A', 'E', 'I', 'O', 'U'].indexOf(v) !== -1){
+            return <button className="vowel" key={v} onClick={props.guessEvent} value={v}>{v}</button>
+          } else {
+            return <button key={v} onClick={props.guessEvent} value={v}>{v}</button>
+          }
+        })}
       </div>
     </div>
   )
